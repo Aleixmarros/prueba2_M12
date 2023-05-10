17bd42db8fa3a2458b436b38c4e9d0f47c52ca7d
Joan Díaz <79135649+joandiaper@users.noreply.github.com>
2023-05-10 20:30:10 +0200

    Update login.component.js

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -1,26 +1,50 @@
-import React, { Component } from 'react'
-import './login.css'
-export default class Login extends Component {
-  render() {
-    return (
-      <form className='form'>
-              <div className="container text-center justify-content-center ">
+import React, { useState, useEffect } from 'react';
+import './login.css';
+
+var vLogin = false;
+
+function Login({ loggedIn, handleLogin }) {
+  const [login, setLogin] = useState(false);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const email = document.querySelector('#email-input').value;
+    const password = document.querySelector('#password-input').value;
+
+    fetch('http://localhost:3000/futmanUsers/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        alert(data.message);
+        setLogin(data.loggedIn);
+        vLogin = data.loggedIn;
+        loggedIn = data.loggedIn;
+        console.log(loggedIn);
+        //onwaiting={handleLogin};
+      })
+      .catch(error => console.error(error));
+  };
+
+  useEffect(() => {
+    // do something with the login state
+  }, [login]);
+
+  return (
+    <form className='form' onSubmit={handleSubmit}>
+      <div className="container text-center justify-content-center">
         <h3>Login</h3><br></br>
         <div className="mb-3">
           <label>Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-          />
+          <input type="email" className="form-control" placeholder="Enter email" id="email-input" required />
         </div>
         <div className="mb-3">
           <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
-          />
+          <input type="password" className="form-control" placeholder="Enter password" id="password-input" required />
         </div>
         <div className="mb-3">
           <div className="custom-control custom-checkbox">
@@ -32,20 +56,22 @@ export default class Login extends Component {
             <label className="custom-control-label" htmlFor="customCheck1">
               Remember me
             </label><br></br>
-          <a href='/sign-up'>Todavia no tienes cuenta?</a>
+            <a href='/sign-up'>Haven't registered yet?</a>
           </div>
         </div>
         <div className="d-grid">
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
+          {loggedIn ? (
+            <p>You are logged in</p>
+          ) : (
+            <button type="submit" className="btn btn-primary" onClick={handleLogin}>Submit</button>
+          )}
+          
         </div>
         <p className="forgot-password text-right">
-          {/* Forgot <a href="#">password?</a> */}
+          Forgot <a href="/forgot-password">password?</a>
         </p>
-        </div>
-
-      </form>
-    )
-  }
+      </div>
+    </form>
+  );
 }
+export default Login;
