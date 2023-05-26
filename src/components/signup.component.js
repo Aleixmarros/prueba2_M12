555ca42bdac2d989d2db725636bbc4e202f29268
Joan Díaz <79135649+joandiaper@users.noreply.github.com>
2023-05-26 18:24:22 +0200

    Update signup.component.js

--- src/components/signup.component.js
+++ src/components/signup.component.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import Dropzone from "react-dropzone";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 export default function SignUp() {
   const [avatar, setAvatar] = useState(null);
@@ -10,8 +12,7 @@ export default function SignUp() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Aquí debes implementar el código para subir la imagen a un servidor o a un servicio de almacenamiento en la nube
-    // y guardar la URL de la imagen en la base de datos de tu aplicación.
+
     const name = document.querySelector('#first-name-input').value;
     const surname = document.querySelector('#last-name-input').value;
     const email = document.querySelector('#email-input').value;
@@ -28,12 +29,15 @@ export default function SignUp() {
       .then(data => {
         console.log(data);
         alert(data.message);
-        // Actualiza la tabla de usuarios con el nuevo registro
       })
       .catch(error => console.error(error));
   };
 
-  
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
 
   return (
     <form className='form' onSubmit={handleSubmit}>
@@ -52,9 +56,21 @@ export default function SignUp() {
           <input type="email" pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z]+(\.[a-zA-Z]{2,})+" className="form-control" placeholder="Enter email (abcde123@gmail.com)" id="email-input" required/>
         </div>
         <div className="mb-3">
-          <label>Password</label>
-          <input type="password" className="form-control" placeholder="Enter password" id="password-input" required/>
-        </div>
+              <label>Password:</label>
+                <input className="form-control" type={showPassword ? 'text' : 'password'} id="password-input"
+                  placeholder="Enter password"
+                  required
+                /><button
+                type="button"
+                className="toggle-password-icon"
+                onMouseDown={handleTogglePassword}
+                onMouseUp={handleTogglePassword}
+              >
+                <FontAwesomeIcon
+                  icon={showPassword ? faEye : faEyeSlash}
+                />
+              </button>
+            </div>
         <form >
           <div className="mb-3">
             <label htmlFor="avatar" className="form-label">
@@ -73,6 +89,9 @@ export default function SignUp() {
               )}
             </Dropzone>
           </div>
+          <button type="submit" className="btn btn-primary" style={{margin: '1vh'}}>
+            Guardar
+          </button>
         </form>
         <div className="d-grid">
           <button type="submit" className="btn btn-primary">
@@ -86,3 +105,4 @@ export default function SignUp() {
     </form>
   );
 }
+
